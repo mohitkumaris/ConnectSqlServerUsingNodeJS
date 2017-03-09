931eb2a007e841936b96e8535698fe1c68f0d964
mohitkumaris <mohitkumaris@gmail.com>
2017-03-09 12:56:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
  var config = {
         user: 'sa',
-        password: 'Welcome@123',
-        server: '192.168.108.194',
-        database: 'ApplicationManager_New' 
+        password: '#####',
+        server: '###.168.108.194',
+        database: 'TestDB' 
     };
 
     module.exports=config;
